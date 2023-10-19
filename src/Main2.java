0ac6968d3db59524210e7406fed450ba64e3908b
robikrobko <112440342+robikrobko@users.noreply.github.com>
2023-10-19 12:40:08 +0200

    druhy subor

---
+++ src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Ahoj Peter, chcem ti iba povedať, že mám rád Javu!");
+        System.out.println("Filip");
+    }
+}
