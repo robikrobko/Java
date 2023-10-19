497cdd752646dbd2bf6a1f63351775884de6084e
robikrobko <112440342+robikrobko@users.noreply.github.com>
2023-10-19 12:56:14 +0200

    piaty subor

---
+++ src/Main5.java
@@ -0,0 +1,15 @@
+
+//a. -5 + 8 * 6
+//b. (55+9) % 9
+//c. 20 + -3*5 / 8
+//d. 5 + 15 / 3 * 2 - 8 % 3
+
+public class Main5 {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 *6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5+15 / 3 * 2 - 8 % 3);
+
+    }
+}
