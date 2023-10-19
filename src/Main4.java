a28ce61fc819294b3e7c1cffafe6490a042360f1
robikrobko <112440342+robikrobko@users.noreply.github.com>
2023-10-19 12:52:56 +0200

    stvrty subor

---
+++ src/Main4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args){
+        int prve = 50;
+        int druhe = 3;
+        System.out.println(prve/druhe);
+        Scanner input = new Scanner(System.in);
+        int prven = input.nextInt();
+        int druhen = input.nextInt();
+        System.out.println(prven/druhen);
+    }
+
+
+}
